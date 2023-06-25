6d60aec15f46c959f8632c279d12a67f551e06a6
Roberto <roberto.antoniello@studenti.unimi.it>
2023-06-25 17:27:15 +0200

    :wrench: translate get config

--- utils/get_config.py
+++ utils/get_config.py
@@ -2,100 +2,100 @@ import utils_config
 from pyrogram import Client
 
 """
-carica il file di configurazione
+Load configuration file
 """
 def get_config_file(json_file):
     config = utils_config.load_config(json_file)
     return utils_config.serialize_config(config)
 
 """
-funzione d'appoggio per inviare messaggi 
+Function that send a text message 
 """
 @Client.on_message()
 def sendMessage(client,message,result):
     client.send_message(get_chat(message),result,disable_web_page_preview=True,reply_to_message_id=get_id_msg(message))
 
 """
-funzione d'appoggio per inviare immagini
+function that send photos
 """
 @Client.on_message()
 def sendPhoto(client,message,result,caption):
     client.send_photo(get_chat(message),result,caption=caption,reply_to_message_id=get_id_msg(message))
 
 """
-funzione d'appoggio per inviare video
+function that send videos
 """
 @Client.on_message()
 def sendVideo(client,message,result,caption):
     client.send_video(get_chat(message),result,caption=caption,file_name='video.mp4',reply_to_message_id=get_id_msg(message))
 
 """
-funzione d'appoggio per inviare audio
+function that send audio
 """
 @Client.on_message()
 def sendAudio(client,message,result,caption):
     client.send_audio(get_chat(message),result,caption=caption,file_name='audio.mp3',reply_to_message_id=get_id_msg(message))
 
 """
-funzione d'appoggio per inviare gif
+function that send gif
 """
 @Client.on_message()
 def sendGIF(client,message,result,caption):
     client.send_animation(get_chat(message),result,caption=caption,reply_to_message_id=get_id_msg(message))
 
 """
-Restituisce l'id utente
+return the user id
 """
 def get_id_user(message):
     try:
         return message.from_user.id
     except:
-        return "id utente non disponibile"
+        return "user id not available"
 """
-Restituisce l'id della chat
+return the chat id
 """
 def get_chat(message):
     try:
         return message.chat.id
     except AttributeError:
-        print("Error durante il prelievo dell'id chat")
+        print("Can't get the chat id")
 
 """
-Restituisce il titolo della chat
+return the chat title
 """
 def get_chat_name(message):
     try:
         return message.chat.title
     except AttributeError:
-        print("Error durante il prelievo del nome della chat")
+        print("can't get the chat name")
 
 """
-Restituisce il nome utente
+return the user first name
 """
 def get_first_name(message):
     try:
         return message.from_user.first_name
     except:
-        return "Nome utente non disponibile"
+        return "User first name not available"
 """
-Restituisce username dell'utente
+return the username 
 """
 def get_username(message):
     try:
         return "@" + message.from_user.username
     except:
-        return "Non impostato"
+        return "Username not existing"
 
 """
-Restituisce il campo testo del messaggio
+return the text message
 """
 def get_text_message(message):
     if message.text is None:
-        return "File multimediale"
+        return "Media File"
     else:
         return message.text
 """
-Restituisce l'id del messaggio
+return the message id
 """
 def get_id_msg(message):
     return message.id
